adf9e8a983f806de87afe4bbb7868be44b3269a9
lutfiqaraman <lutfiqaraman@gmail.com>
2021-11-19 21:04:39 +0200

    build entity class

---
+++ DDDInPractice.Logic/Entity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDDInPractice.Logic
+{
+    public abstract class Entity
+    {
+        public long Id { get; private set; }
+        public override bool Equals(object obj)
+        {
+            Entity other = obj as Entity;
+
+            if (ReferenceEquals(other, null))
+                return false;
+
+            return Id == other.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
