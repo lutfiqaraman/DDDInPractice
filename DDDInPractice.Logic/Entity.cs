52c2ed6152549036e8af0dda017404d16ee16d34
lutfiqaraman <lutfiqaraman@gmail.com>
2021-11-20 19:53:48 +0200

    create Entity class

--- DDDInPractice.Logic/Entity.cs
+++ DDDInPractice.Logic/Entity.cs
@@ -9,6 +9,7 @@ namespace DDDInPractice.Logic
     public abstract class Entity
     {
         public long Id { get; private set; }
+
         public override bool Equals(object obj)
         {
             Entity other = obj as Entity;
@@ -16,11 +17,38 @@ namespace DDDInPractice.Logic
             if (ReferenceEquals(other, null))
                 return false;
 
+            if (ReferenceEquals(this, other))
+                return true;
+
+            if (GetType() != other.GetType())
+                return false;
+
+            if (Id == 0 || other.Id == 0)
+                return false;
+
+
             return Id == other.Id;
         }
+
+        public static bool operator ==(Entity a, Entity b)
+        {
+            if (ReferenceEquals(a, null) && ReferenceEquals(b, null))
+                return true;
+
+            if (ReferenceEquals(a, null) || ReferenceEquals(b, null))
+                return false;
+
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(Entity a, Entity b)
+        {
+            return !(a == b);
+        }
+
         public override int GetHashCode()
         {
-            return base.GetHashCode();
+            return (GetType().ToString() + Id).GetHashCode();
         }
     }
 }
