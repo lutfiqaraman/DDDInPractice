be696d4220c5e6f205822bb2a7253a71c809a732
lutfiqaraman <lutfiqaraman@gmail.com>
2021-11-21 21:19:02 +0200

    create value object

---
+++ DDDInPractice.Logic/ValueObject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDDInPractice.Logic
+{
+    public abstract class ValueObject<T> where T :ValueObject<T>
+    {
+
+    }
+}
